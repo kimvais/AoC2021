4f6d0e024ba715dc54b7a93a6fc5a7150d49bdb7
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2021-12-06 17:38:53 +0200

    Switch tests to use FsUnit

--- Tests.fs
+++ Tests.fs
@@ -1,40 +1,52 @@
 module AoC2021.Tests
+
+open FsUnit.Xunit
 open Xunit
-open Xunit.Abstractions
 
 open AoC2021.Day1
 open AoC2021.Day2
 open AoC2021.Day3
 open AoC2021.Day4
 open AoC2021.Day5
+open AoC2021.Day6
 
-type Puzzles(o: ITestOutputHelper) =
-    [<Fact>]
-    let ``day 1`` () =
-        Assert.Equal(7I, day1 "test1" ())
-        Assert.Equal(5I, day1part2 "test1" ())
-        Assert.Equal(1266I, day1 "1" ())
-        Assert.Equal(1217I, day1part2 "1" ())
+[<Fact>]
+let ``day 1`` () =
+    day1 "test1" () |> should equal 7L
+    day1part2 "test1" () |> should equal 5L
+    day1 "1" () |> should equal 1266L
+    day1part2 "1" () |> should equal 1217L
 
-    [<Fact>]
-    let ``day2 `` () =
-        Assert.Equal(150I, day2 "test2" ())
-        Assert.Equal(1804520I, day2 "2" ())
-        Assert.Equal(900I, day2part2 "test2" ())
-        Assert.Equal(1971095320I, day2part2 "2" ())
-        
-    [<Fact>]
-    let ``day3 `` () =
-        Assert.Equal(198I, day3 "test3" ())
-        Assert.Equal(4160394I, day3 "3" ())
-        Assert.Equal(230I, day3part2 "test3" ())
-        
-    [<Fact>]
-    let ``day 4`` () =
-        Assert.Equal(4512I, day4 "test4" ())
-        Assert.Equal(54275I, day4 "4" ())
-        Assert.Equal(1924I, day4part2 "test4" ())
-        
-    let ``day 5``() =
-        Assert.Equal(5I, day5 "test5" ())
-        Assert.Equal(6666I, day5 "5" ())
+[<Fact>]
+let ``day2 `` () =
+    day2 "test2" () |> should equal 150L
+    day2 "2" () |> should equal 1804520L
+    day2part2 "test2" () |> should equal 900L
+    day2part2 "2" () |> should equal 1971095320L
+
+[<Fact>]
+let ``day3 `` () =
+    day3 "test3" () |> should equal 198L
+    day3 "3" () |> should equal 4160394L
+    day3part2 "test3" () |> should equal 230L
+
+[<Fact>]
+let ``day 4`` () =
+    day4 "test4" () |> should equal 4512L
+    day4 "4" () |> should equal 54275L
+    day4part2 "test4" () |> should equal 1924L
+
+[<Fact>]
+let ``day 5`` () =
+    day5 "test5" () |> should equal 5L
+    day5 "5" () |> should equal 6666L
+    // day5part2 "test5" |> should equal 12L
+    // day5part2 "5" |> should equal 
+
+[<Fact>]
+let ``day 6`` () =
+    day6 "test6" 18 () |> should equal 26L
+    day6 "test6" 80 () |> should equal 5934L
+    day6 "6" 80 () |> should equal 343441L
+    day6 "test6" 256 () |> should equal 26984457539L
+    day6 "6" 256 () |> should equal 1569108373832L
