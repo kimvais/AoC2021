18a8715a06f948b97caf80191ea20bcda05bfd6a
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2021-12-10 12:15:53 +0200

    Day 10 part 2, WIP

--- Day10.fs
+++ Day10.fs
@@ -16,6 +16,13 @@ let score =
     | '}' -> 1197L
     | '>' -> 25137L
 
+let score2 =
+    function
+    | ')' -> 1L
+    | ']' -> 2L
+    | '}' -> 3L 
+    | '>' -> 4L 
+    
 let opening = "({[<" |> Set.ofSeq
 
 let rec check stack chars =
@@ -33,6 +40,23 @@ let rec check stack chars =
                 match matchClosing c = head with
                 | false -> Some head
                 | true -> check (List.tail stack) tail
+                
+let rec check2 stack chars =
+    let c = List.tryHead stack
+
+    match chars with
+    | [] -> Some stack
+    | head :: tail ->
+        match c with
+        | None -> check2 [ head ] tail
+        | Some c ->
+            match Set.contains head opening with
+            | true -> check2 ([ head ] @ stack) tail
+            | false ->
+                match matchClosing c = head with
+                | false -> None
+                | true -> check2 (List.tail stack) tail
+
 
 let day10 fn () =
     let input = readInput fn |> Seq.map List.ofSeq
@@ -41,8 +65,10 @@ let day10 fn () =
     |> Seq.choose (check [])
     |> Seq.sumBy score
 
+let folder tally n = tally * 5L + n
 
 let day10part2 fn () =
     let input = readInput fn |> Seq.map List.ofSeq
-    input |> Seq.filter (check [] >> function |Some _ -> false | None -> true) |> printfn "%A"
+    input |> Seq.filter (check [] >> function |Some _ -> false | None -> true) |> Seq.choose (check2 []) |> Seq.map (Seq.map (matchClosing >> score2) )
+    |> printfn "%A"
     0L
